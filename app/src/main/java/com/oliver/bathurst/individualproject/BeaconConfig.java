5d207e52f04d43db98e103bc4ab8a402fa4ca64b
Oliver <oliverbathurst12345@gmail.com>
2017-10-23 16:19:22 +0100

    Finished beacon configure page design

--- app/src/main/java/com/oliver/bathurst/individualproject/BeaconConfig.java
+++ app/src/main/java/com/oliver/bathurst/individualproject/BeaconConfig.java
@@ -2,11 +2,14 @@ package com.oliver.bathurst.individualproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class BeaconConfig extends AppCompatActivity {
-
+    private int selectedPosition = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +17,22 @@ public class BeaconConfig extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_beacon_config);
+
+        ((Spinner) findViewById(R.id.spinner)).setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selectedPosition = position;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        (findViewById(R.id.saveBeacon)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
