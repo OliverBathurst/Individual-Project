8b577ba09c44b47a31b3d24e9ce0f0fdb6a34f4d
Oliver <oliverbathurst12345@gmail.com>
2017-12-11 16:52:35 +0000

    Fixed data structure syntax that only allowed one pair to exist in Fingerprint helper class

--- app/src/main/java/com/oliver/bathurst/individualproject/WiFiFingerprinter.java
+++ app/src/main/java/com/oliver/bathurst/individualproject/WiFiFingerprinter.java
@@ -1,6 +1,21 @@
 package com.oliver.bathurst.individualproject;
 
+import static android.content.Context.WIFI_SERVICE;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.preference.PreferenceManager;
+import android.util.Pair;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Created by Oliver on 11/12/2017.
  * Written by Oliver Bathurst <oliverbathurst12345@gmail.com>
@@ -8,20 +23,52 @@ import android.content.Context;
 
 class WiFiFingerprinter {
     private final Context c;
+    private boolean isFinished = false;
+    private String response;
+    private WifiManager wifiMan;
+    private ArrayList<Pair<String, HashMap<String, Integer>>> fromPrefs;
 
     WiFiFingerprinter(Context context){
         this.c = context;
     }
 
+    String getResults(){
+        startScan();
+        while (!isFinished){}
+        return response;
+    }
 
+    private void compare(List<ScanResult> results){
+        HashMap<String, Integer> points = new HashMap<>();
+        for(Pair<String, HashMap<String, Integer>> alias: fromPrefs){
+            for(ScanResult scanned: results){
 
+            }
+        }
+    }
+    private void startScan(){
+        String doesExist = PreferenceManager.getDefaultSharedPreferences(c).getString("WIFI_PRINTS", null);
+        if(doesExist != null) {
 
+            fromPrefs = new Gson().fromJson(doesExist, new TypeToken<ArrayList<Pair<String, HashMap<String, Integer>>>>() {}.getType());
+            wifiMan = (WifiManager) c.getApplicationContext().getSystemService(WIFI_SERVICE);
 
+            if(wifiMan != null){
+                c.getApplicationContext().registerReceiver(new WifiReceiver(), new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+                wifiMan.startScan();
+            }else{
+                response = c.getString(R.string.wifi_not_supported);
+                isFinished = true;
+            }
+        }else{
+            response = c.getString(R.string.no_fingerprints_found);
+            isFinished = true;
+        }
+    }
 
-
-
-
-
-
-
+    class WifiReceiver extends BroadcastReceiver {
+        public void onReceive(Context c, Intent intent) {
+            compare(wifiMan.getScanResults());
+        }
+    }
 }
--- app/src/main/java/com/oliver/bathurst/individualproject/WiFiScanner.java
+++ app/src/main/java/com/oliver/bathurst/individualproject/WiFiScanner.java
@@ -13,23 +13,23 @@ import android.util.Pair;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 public class WiFiScanner extends AppCompatActivity {
     private static final int TOTAL_SCANS = 10;
     private int scans = 0;
     private String aliasString;
-    private TextView alias;
+    private TextView alias, progressText;
     private ProgressBar progress;
     private WifiManager wifiMan;
     private WifiReceiver broadcast;
-    private ArrayList<Pair<String, Integer>> wifiScanList;
+    private ArrayList<Pair<String, Integer>> wifiScanList; //stores SSIDs and their signal strengths
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +38,7 @@ public class WiFiScanner extends AppCompatActivity {
         setTitle(getString(R.string.wifi_scanner_title));
 
         alias = (TextView) findViewById(R.id.aliasWiFi);
+        progressText = (TextView) findViewById(R.id.progressUpdate);
         wifiMan = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
         progress = (ProgressBar) findViewById(R.id.progressFingerprint);
 
@@ -48,6 +49,7 @@ public class WiFiScanner extends AppCompatActivity {
                     aliasString = alias.getText().toString().trim();
                     if(aliasString.length() > 0) {
                         if (!wifiMan.isWifiEnabled()) {
+                            progressText.setText(R.string.enabling_wifi);
                             wifiMan.setWifiEnabled(true);
                         }
                         wifiScanList = new ArrayList<>();
@@ -55,29 +57,43 @@ public class WiFiScanner extends AppCompatActivity {
                         registerReceiver(broadcast, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
                         wifiMan.startScan();
                     }else{
-                        Toast.makeText(getApplicationContext(), "Alias cannot be blank", Toast.LENGTH_SHORT).show();
+                        progressText.setText(R.string.blank_alias);
                     }
                 }else{
-                    Toast.makeText(getApplicationContext(), "WiFi not supported", Toast.LENGTH_SHORT).show();
+                    progressText.setText(R.string.wifi_not_supported);
                 }
             }
         });
     }
     private void reduceAndSave(){
+        progressText.setText(R.string.reducing_and_saving);
         HashMap<String, Integer> storage = new HashMap<>();
         for(int i = 0; i < wifiScanList.size(); i++){
-            Integer temp = storage.get(wifiScanList.get(i).first);
-            if(temp != null) {
-                storage.put(wifiScanList.get(i).first, ((temp + wifiScanList.get(i).second)/2));
+            Integer temp = storage.get(wifiScanList.get(i).first); //get the value associated with the SSID String key
+            if(temp != null) { //if the value associated with the key already exists...
+                storage.put(wifiScanList.get(i).first, ((temp + wifiScanList.get(i).second)/2)); // add the new value to the old one and halve to average, write back to the same key
             }else{
-                storage.put(wifiScanList.get(i).first, wifiScanList.get(i).second);
+                storage.put(wifiScanList.get(i).first, wifiScanList.get(i).second); //otherwise place in hashmap
             }
         }
-        PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit().putString("WIFI_PRINTS", new Gson().toJson(storage)).apply();
-        Toast.makeText(getApplicationContext(), "Finished", Toast.LENGTH_SHORT).show();
+        //store alias along with a hashmap of SSIDs and averaged signal strengths
+
+        ArrayList<Pair<String, HashMap<String, Integer>>> toStore;
+        String doesExist = PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("WIFI_PRINTS", null);
+        if(doesExist != null){
+            toStore = new Gson().fromJson(doesExist, new TypeToken<ArrayList<Pair<String, HashMap<String, Integer>>>>() {}.getType());
+            toStore.add(new Pair<>(aliasString, storage));
+            PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit().putString("WIFI_PRINTS", new Gson().toJson(toStore)).apply();
+        }else{
+            toStore = new ArrayList<>();
+            toStore.add(new Pair<>(aliasString, storage));
+            PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit().putString("WIFI_PRINTS", new Gson().toJson(toStore)).apply();
+        }
+        progressText.setText(R.string.finished);
     }
     private void updateStats(){
         scans++;
+        progressText.setText(String.format(Locale.UK, "%s%d%s%d", getString(R.string.scans_finished), scans, getString(R.string.slash), TOTAL_SCANS));
         progress.setProgress((scans/TOTAL_SCANS) * 100);
         wifiMan.startScan();
     }
